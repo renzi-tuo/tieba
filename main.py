3770f8fc3d3fdb3c445c5d59488fb6569fa9e828
renzi-tuo <58037373+renzi-tuo@users.noreply.github.com>
2020-08-24 16:23:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -170,16 +170,14 @@ def client_sign(bduss, tbs, fid, kw):
 
 def main():
     b = os.environ['BDUSS'].split('#')
-    desp = []
     for n, i in enumerate(b):
         logger.info("开始签到第" + str(n) + "个用户")
-        desp.append(logger.info("开始签到第" + str(n) + "个用户"))
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         for j in favorites:
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
-        desp.append(logger.info("完成第" + str(n) + "个用户签到"))
+        desp = logger.info("完成第" + str(n) + "个用户签到")
     logger.info("所有用户签到结束")
     socky= os.environ['PUSH_KEY']
     ISOTIMEFORMAT = '%Y-%m-%d %H:%M'
